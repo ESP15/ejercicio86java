6d94e07e12f430ecfba223d2f5e385009210f99a
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-04-04 13:39:48 +0200

    Add files via upload

---
+++ ejercicio86java/main.js
@@ -0,0 +1,23 @@
+// function replaceLetters(str) {
+//     let newStr = "";
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === "A") {
+//             newStr += "O";
+//         } else {
+//             newStr += str[i];
+//         }
+//     }
+//     return newStr;
+// }
+
+
+let a ;
+var b;
+const c = "a";
+var b;
+
+console.log(typeof(c));
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
